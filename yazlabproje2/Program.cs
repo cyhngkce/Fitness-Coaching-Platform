2d56fc299451114bf715e2d4c588f89e6c2099e3
Kaan Kutay KÖSE <110459413+kaankutaykose@users.noreply.github.com>
2024-07-12 13:38:09 +0300

    Update Program.cs

--- yazlabproje2/Program.cs
+++ yazlabproje2/Program.cs
@@ -18,24 +18,3 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddControllersWithViews();
 
 
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-}
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapRazorPages();
-
-app.Run();
